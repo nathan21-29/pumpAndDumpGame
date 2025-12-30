11d45077a31fb7fd23f6ddbd6ee9c47597551dcf
Thiccblah <oofey123@gmail.com>
2025-12-30 00:20:16 -0500

    sync

--- pumpAndDumpGame/src/classFiles/Player.java
+++ pumpAndDumpGame/src/classFiles/Player.java
@@ -4,12 +4,14 @@ import java.util.*;
 
 public class Player {
 
+	private String name;
 	private double money;
 	private double quota;
 	private double quotaProgress;
 	private ArrayList<Holding> portfolio;
 
-	public Player (double money, double quota) {
+	public Player (String name, double money, double quota) {
+		this.name = name;
 		this.money = money;
 		this.quota = quota;
 		this.quotaProgress = 0;
